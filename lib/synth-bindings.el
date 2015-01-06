a7fb6732e4683eaf861e80bfcb33f17f55eb8a42
Chris Blom <chris.blom@millmobile.com>
2015-01-06 14:58:08 +0100

    fancy chord for window selection

--- lib/synth-bindings.el
+++ lib/synth-bindings.el
@@ -11,7 +11,7 @@
 ;; global bindings
 (bind-keys
  ("C-h"       . backward-delete-char)
- ("s-w"       . backward-kill-word) ; like in terminals
+ ("s-w"       . backward-kill-word)
  ("M-<left>"  . backward-word)
  ("M-<right>" . forward-word)
  ("M-SPC"     . live-delete-whitespace-except-one)
@@ -29,3 +29,15 @@
  ("C-c m d" . eval-defun)
  ("C-c m n" . eval-print-last-sexp)
  ("C-c m r" . eval-region))
+
+
+(key-chord-define-global "1q" (lambda () (interactive) (window-number-select 1)))
+(key-chord-define-global "2w" (lambda () (interactive) (window-number-select 2)))
+(key-chord-define-global "3e" (lambda () (interactive) (window-number-select 3)))
+(key-chord-define-global "4r" (lambda () (interactive) (window-number-select 4)))
+(key-chord-define-global "5t" (lambda () (interactive) (window-number-select 5)))
+(key-chord-define-global "6y" (lambda () (interactive) (window-number-select 6)))
+(key-chord-define-global "7u" (lambda () (interactive) (window-number-select 7)))
+(key-chord-define-global "8i" (lambda () (interactive) (window-number-select 8)))
+(key-chord-define-global "9o" (lambda () (interactive) (window-number-select 9)))
+(key-chord-define-global "0p" (lambda () (interactive) (window-number-select 10)))
